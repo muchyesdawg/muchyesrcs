71d4d4e757044475b5982582f5ac8d511a79ae15
muchyesdawg <markusau0607@gmail.com>
2025-01-04 14:01:17 -0600

    idk

--- installScript.sh
+++
@@ -1,4 +0,0 @@
-sudo pacman -S opera git github-cli chromium discord yay otf-codenewroman-nerd neovim ccls box2d sfml zsh tree bluez cmake
-yay -S spotify-edge spotify-adblock-git ani-cli-git 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-
